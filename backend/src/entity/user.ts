97e4c349c9af044ff93970029ed9919315c1f6a7
CarlosFonseca75 <cardfonseca07@gmail.com>
2025-07-30 18:39:50 -0600

    feat: :sparkles: Adding user entity.

---
+++ backend/src/entity/user.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, length: 100 })
+  email: string;
+
+  @Column({ length: 50 })
+  firstName: string;
+
+  @Column({ length: 50 })
+  lastName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
