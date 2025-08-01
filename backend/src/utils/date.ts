5da12b02403b7d4adbd25df7e0899a15a71f832d
CarlosFonseca75 <cardfonseca07@gmail.com>
2025-08-01 09:42:47 -0600

    feat: :sparkles: Adding dayjs dependency and helpers to handle times.

---
+++ backend/src/utils/date.ts
@@ -0,0 +1,21 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+// TODO: We could implement an adapter patter to include more dates dependencies in the future.
+export class Dayjs {
+  static nowUtc(): Date {
+    return dayjs().utc().toDate();
+  }
+
+  static toUtc(date: Date | string): Date {
+    return dayjs(date).utc().toDate();
+  }
+
+  static format(date: Date | string, format = "YYYY-MM-DD HH:mm:ss"): string {
+    return dayjs(date).utc().format(format);
+  }
+}
