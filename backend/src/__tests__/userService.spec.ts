c9f388a727dcb2251a69a93cbdd4211b02a65997
CarlosFonseca75 <cardfonseca07@gmail.com>
2025-08-04 09:16:09 -0600

    test: :test_tube: Adding get all users unit test.

--- backend/src/__tests__/userService.spec.ts
+++ backend/src/__tests__/userService.spec.ts
@@ -1,10 +1,46 @@
 import { UserService } from "../services/userService";
 import { UserRepository } from "../repositories/userRepository";
+import { Dayjs } from "../utils/date";
+import { HttpStatus } from "../common/enums";
 
 const mockFind = jest.fn();
 
+jest.mock("../repositories/userRepository", () => {
+  return {
+    UserRepository: jest.fn().mockImplementation(() => ({
+      find: mockFind,
+    })),
+  };
+});
+
 describe("UserService", () => {
-  it("should return a list of users", () => {
-    expect(200).toBe(200);
+  let userService: UserService;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const userRepository = new UserRepository();
+    userService = new UserService(userRepository);
+  });
+
+  it("should return a list of users", async () => {
+    const fakeUsers = [
+      {
+        id: "1",
+        email: "carlos@example.com",
+        firstName: "Carlos Antonio",
+        lastName: "Díaz Fonseca",
+        createdAt: Dayjs.nowUtc(),
+        updatedAt: Dayjs.nowUtc(),
+      },
+    ];
+
+    mockFind.mockResolvedValue(fakeUsers);
+
+    const result = await userService.getUsers();
+
+    expect(mockFind).toHaveBeenCalledTimes(1);
+    expect(result.status).toBe(HttpStatus.OK);
+    expect(result.data).toEqual(fakeUsers);
+    expect(result.data.length).toBe(1);
   });
 });
